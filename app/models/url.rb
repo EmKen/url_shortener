bfd6295024959583d44d1c5f7e07dc0a4f5011cf
EmKen <emily.kennedy_95@hotmail.co.uk>
2017-04-25 17:24:50 +0800

    shorten method implemented

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,7 +2,8 @@ class Url < ApplicationRecord
 	before_create do
 		self.short_url = shorten
 	end
-	validates :long_url, format: { with: /https://.+/, message: "Url must begin with https://"}
+	validates :long_url, format: { with: %r{https://.+}i, message: "Url must begin with https://"}
+	
 	def shorter
 		(('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a).sample(rand(2)+6).join
 	end
