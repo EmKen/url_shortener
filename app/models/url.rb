b970f644351f414e9256c5053a5dcc4742c1e015
EmKen <emily.kennedy_95@hotmail.co.uk>
2017-04-25 13:19:58 +0800

    Defined url shortener method and stipulated validations within url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,17 @@
 class Url < ApplicationRecord
+	before_create do
+		self.short_url = shorten
+	end
+	validates :long_url, format: { with: /https://.+/, message: "Url must begin with https://"}
+	def shorter
+		(('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a).sample(rand(2)+6).join
+	end
+
+	def shorten
+		short = shorter
+		while Url.exists?(:short_url => short)
+			short = shorter
+		end
+		short
+	end
 end
